cccdd7abbff3f1d6901d3f6d1ea8dc515756813b
Jan Judas <jan.judas@resistant.ai>
2024-11-17 13:25:02 +0100

    macro for running a part

--- src/main/scala/cz/judas/jan/advent/main.scala
+++ src/main/scala/cz/judas/jan/advent/main.scala
@@ -2,6 +2,6 @@ package cz.judas.jan.advent
 
 import cz.judas.jan.advent.year2021.Day03
 
-@main def hello(): Unit =
-  val input = InputData.real(year = 2021, day = 3)
-  println(Day03.part2(input))
+@main def entrypoint(): Unit =
+  val result = run(year = 2021, day = 3, part = 2)
+  println(result)
---
+++ src/main/scala/cz/judas/jan/advent/runner.scala
@@ -0,0 +1,24 @@
+package cz.judas.jan.advent
+
+import scala.quoted.{Expr, Quotes}
+
+
+inline def run(inline year: Int, inline day: Int, inline part: Int): Any =
+  ${ runImpl('year, 'day, 'part) }
+
+def runImpl(year: Expr[Int], day: Expr[Int], part: Expr[Int])(using q: Quotes): Expr[Any] =
+  import q.reflect.*
+  val constYear = extractConstant(year)
+  val constDay = extractConstant(day)
+  val constPart = extractConstant(part)
+
+  Apply(
+    Select.unique(Ref(Symbol.requiredModule(f"cz.judas.jan.advent.year${constYear}.Day$constDay%02d")), s"part${constPart}"),
+    List('{ InputData.real(${year}, ${day}) }.asTerm),
+  ).asExprOf[Any]
+
+def extractConstant(value: Expr[Int])(using q: Quotes): Int =
+  import q.reflect.*
+  value.asTerm match
+    case Inlined(_, _, Literal(IntConstant(constValue))) => constValue
+    case _ => report.errorAndAbort("Expected constant value")
