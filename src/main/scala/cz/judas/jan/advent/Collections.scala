33e084c83667e715eafacfcb3a95062f0202a07e
Jan Judas <jan.judas@resistant.ai>
2024-12-01 07:21:41 +0100

    make it a bit nicer

---
+++ src/main/scala/cz/judas/jan/advent/Collections.scala
@@ -0,0 +1,45 @@
+package cz.judas.jan.advent
+
+import scala.collection.mutable
+
+
+class AutoMap[K, V](valueFactory: K => V):
+  private val values = mutable.HashMap[K, V]()
+
+  def getOrCreate(key: K): V =
+    values.get(key) match
+      case Some(value) => value
+      case None =>
+        val value = valueFactory(key)
+        values.put(key, value)
+        value
+
+  def put(key: K, value: V): Unit =
+    values.put(key, value)
+
+  def toMap: Map[K, V] =
+    values.toMap
+
+
+object AutoMap:
+  def apply[K, V](valueFactory: K => V): AutoMap[K, V] =
+    new AutoMap(valueFactory)
+
+  def apply[K, V](valueFactory: => V): AutoMap[K, V] =
+    new AutoMap(k => valueFactory)
+
+
+extension[A] (values: IterableOnce[A])
+  def histogram: Map[A, Int] =
+    val result = AutoMap[A, Int](0)
+    values.iterator.foreach(value => result.put(value, result.getOrCreate(value) + 1))
+    result.toMap
+
+
+extension[A, B] (values: IterableOnce[(A, B)])
+  def toSeqs: (Seq[A], Seq[B]) =
+    val list = values.iterator.toIndexedSeq
+    (
+      list.map(_._1),
+      list.map(_._2)
+    )
--- src/main/scala/cz/judas/jan/advent/year2024/Day01.scala
+++ src/main/scala/cz/judas/jan/advent/year2024/Day01.scala
@@ -1,32 +1,29 @@
 package cz.judas.jan.advent.year2024
 
-import cz.judas.jan.advent.InputData
-
-import scala.collection.mutable
-import scala.math.abs
+import cz.judas.jan.advent.{InputData, histogram, pattern, toSeqs}
 
 object Day01:
   def part1(input: InputData): Int =
-    val values = input.lines
-      .map(line =>
-        val parts = line.split("\\s+")
-        (parts(0).toInt, parts(1).toInt)
-      )
-      .toList
+    val (leftValues, rightValues) = input.linesAs[ValuePair]
+      .map(_.asTuple)
+      .toSeqs
 
-    values.map(pair => pair(0)).sorted
-      .zip(values.map(pair => pair(1)).sorted)
-      .map((value1, value2) => abs(value2 - value1))
+    leftValues.sorted
+      .zip(rightValues.sorted)
+      .map((value1, value2) => (value2 - value1).abs)
       .sum
 
   def part2(input: InputData): Int =
-    val values = input.lines
-      .map(line =>
-        val parts = line.split("\\s+")
-        (parts(0).toInt, parts(1).toInt)
-      )
-      .toList
+    val (leftValues, rightValues) = input.linesAs[ValuePair]
+      .map(_.asTuple)
+      .toSeqs
 
-    val histogram = mutable.HashMap[Int, Int]()
-    values.map((_, value2) => value2).foreach(value => histogram.put(value, histogram.getOrElse(value, 0) + 1))
-    values.map((value1, _) => value1 * histogram.getOrElse(value1, 0)).sum
+    val histogram = rightValues.histogram
+    leftValues.map(value => value * histogram.getOrElse(value, 0))
+      .sum
+
+
+@pattern("{}   {}")
+case class ValuePair(value1: Int, value2: Int):
+  def asTuple: (Int, Int) =
+    (value1, value2)
