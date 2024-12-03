c9fd188063405ef4f64edd71065ab187bcadcc28
Jan Judas <jan.judas@resistant.ai>
2024-12-03 08:31:49 +0100

    merge into one regex

--- src/main/scala/cz/judas/jan/advent/year2024/day03.scala
+++ src/main/scala/cz/judas/jan/advent/year2024/day03.scala
@@ -6,19 +6,15 @@ import java.util.regex.Pattern
 
 object Day03:
   def part1(input: InputData): Int =
-    val pattern = Pattern.compile("mul\\([0-9]{1,3},[0-9]{1,3}\\)")
-    val pattern2 = Pattern.compile("mul\\(([0-9]{1,3}),([0-9]{1,3})\\)")
+    val pattern = Pattern.compile("mul\\(([0-9]{1,3}),([0-9]{1,3})\\)")
     val matcher = pattern.matcher(input.whole)
     var result = 0
     while matcher.find() do
-      val matcher2 = pattern2.matcher(matcher.group(0))
-      matcher2.find()
-      result += matcher2.group(1).toInt * matcher2.group(2).toInt
+      result += matcher.group(1).toInt * matcher.group(2).toInt
     result
 
   def part2(input: InputData): Int =
-    val pattern = Pattern.compile("mul\\([0-9]{1,3},[0-9]{1,3}\\)|do\\(\\)|don't\\(\\)")
-    val pattern2 = Pattern.compile("mul\\(([0-9]{1,3}),([0-9]{1,3})\\)")
+    val pattern = Pattern.compile("mul\\(([0-9]{1,3}),([0-9]{1,3})\\)|do\\(\\)|don't\\(\\)")
     val matcher = pattern.matcher(input.whole)
     var result = 0
     var enabled = true
@@ -30,7 +26,5 @@ object Day03:
         enabled = true
       else
         if enabled then
-          val matcher2 = pattern2.matcher(instruction)
-          matcher2.find()
-          result += matcher2.group(1).toInt * matcher2.group(2).toInt
+          result += matcher.group(1).toInt * matcher.group(2).toInt
     result
