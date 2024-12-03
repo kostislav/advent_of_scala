2622b727a4414b61bb94db56e0cb8e63b4215d44
Jan Judas <jan.judas@resistant.ai>
2024-12-03 08:34:30 +0100

    correct test inputs

--- src/test/scala/cz/judas/jan/advent/year2024/Day03Test.scala
+++ src/test/scala/cz/judas/jan/advent/year2024/Day03Test.scala
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Test
 import org.scalatest.Assertions.assert
 
 class Day03Test:
-  private val input = InputData.fromString("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
-
   @Test
   def part1Works(): Unit =
+    val input = InputData.fromString("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
+
     assert(Day03.part1(input) == 161)
 
   @Test
   def part2Works(): Unit =
+    val input = InputData.fromString("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
+
     assert(Day03.part2(input) == 48)
