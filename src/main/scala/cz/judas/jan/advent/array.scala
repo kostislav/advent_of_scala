bac8defd8fac81708fcb55a77f2804905cb95408
Jan Judas <jan.judas@resistant.ai>
2024-12-04 07:51:40 +0100

    move reusable things

---
+++ src/main/scala/cz/judas/jan/advent/array.scala
@@ -0,0 +1,45 @@
+package cz.judas.jan.advent
+
+import scala.annotation.targetName
+
+
+case class Position(row: Int, column: Int):
+  @targetName("plus")
+  def +(offset: RelativePosition): Position =
+    Position(row + offset.rowOffset, column + offset.columnOffset)
+
+  @targetName("minus")
+  def -(offset: RelativePosition): Position =
+    Position(row - offset.rowOffset, column - offset.columnOffset)
+
+
+case class RelativePosition(rowOffset: Int, columnOffset: Int):
+  @targetName("times")
+  def *(multiplier: Int): RelativePosition =
+    RelativePosition(rowOffset * multiplier, columnOffset * multiplier)
+
+object RelativePosition:
+  def allDirections: Seq[RelativePosition] =
+    fromTuples(Seq((1, 0), (0, 1), (1, 1), (1, -1), (-1, 0), (0, -1), (-1, -1), (-1, 1)))
+
+  def diagonalDirections: Seq[RelativePosition] =
+    fromTuples(Seq((1, 1), (1, -1), (-1, -1), (-1, 1)))
+
+  private def fromTuples(tuples: Seq[(Int, Int)]): Seq[RelativePosition] =
+    tuples.map(tuple => RelativePosition(tuple._1, tuple._2))
+
+
+class Array2d private(rows: IndexedSeq[String], val numRows: Int, val numColumns: Int):
+  def get(position: Position): Option[Char] =
+    if position.row >= 0 && position.row < numRows && position.column >= 0 && position.column < numColumns then
+      Some(rows(position.row).charAt(position.column))
+    else
+      None
+
+  def indices: Iterator[Position] =
+    (0 until numRows).iterator.flatMap(column => (0 until numColumns).map(row => Position(row, column)))
+
+object Array2d:
+  def fromInput(input: InputData): Array2d =
+    val rows = input.lines.toIndexedSeq
+    Array2d(rows, rows.size, rows(0).length)
--- src/main/scala/cz/judas/jan/advent/year2024/day04.scala
+++ src/main/scala/cz/judas/jan/advent/year2024/day04.scala
@@ -1,8 +1,6 @@
 package cz.judas.jan.advent.year2024
 
-import cz.judas.jan.advent.{InputData, cartesianProduct}
-
-import scala.annotation.targetName
+import cz.judas.jan.advent.*
 
 object Day04:
   def part1(input: InputData): Int =
@@ -36,45 +34,3 @@ class WordSearch(letters: Array2d):
 object WordSearch:
   def fromInput(input: InputData): WordSearch =
     WordSearch(Array2d.fromInput(input))
-
-
-case class Position(row: Int, column: Int):
-  @targetName("plus")
-  def +(offset: RelativePosition): Position =
-    Position(row + offset.rowOffset, column + offset.columnOffset)
-
-  @targetName("minus")
-  def -(offset: RelativePosition): Position =
-    Position(row - offset.rowOffset, column - offset.columnOffset)
-
-
-case class RelativePosition(rowOffset: Int, columnOffset: Int):
-  @targetName("times")
-  def *(multiplier: Int): RelativePosition =
-    RelativePosition(rowOffset * multiplier, columnOffset * multiplier)
-
-object RelativePosition:
-  def allDirections: Seq[RelativePosition] =
-    fromTuples(Seq((1, 0), (0, 1), (1, 1), (1, -1), (-1, 0), (0, -1), (-1, -1), (-1, 1)))
-
-  def diagonalDirections: Seq[RelativePosition] =
-    fromTuples(Seq((1, 1), (1, -1), (-1, -1), (-1, 1)))
-
-  private def fromTuples(tuples: Seq[(Int, Int)]): Seq[RelativePosition] =
-    tuples.map(tuple => RelativePosition(tuple._1, tuple._2))
-
-
-class Array2d private(rows: IndexedSeq[String], val numRows: Int, val numColumns: Int):
-  def get(position: Position): Option[Char] =
-    if position.row >= 0 && position.row < numRows && position.column >= 0 && position.column < numColumns then
-      Some(rows(position.row).charAt(position.column))
-    else
-      None
-
-  def indices: Iterator[Position] =
-    (0 until numRows).iterator.flatMap(column => (0 until numColumns).map(row => Position(row, column)))
-
-object Array2d:
-  def fromInput(input: InputData): Array2d =
-    val rows = input.lines.toIndexedSeq
-    Array2d(rows, rows.size, rows(0).length)
