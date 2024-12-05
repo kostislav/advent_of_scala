a766c0567bb0c59ae382624b37f33af741efdbc7
Jan Judas <jan.judas@resistant.ai>
2024-12-05 08:03:40 +0100

    deduplicate

--- src/main/scala/cz/judas/jan/advent/year2024/day05.scala
+++ src/main/scala/cz/judas/jan/advent/year2024/day05.scala
@@ -6,36 +6,12 @@ import scala.collection.mutable
 
 object Day05:
   def part1(input: InputData): Int =
-    val Array(orderingRulesString, updateString) = input.whole.split("\n\n")
-    val orderingRulesBuilder = AutoMap[Int, mutable.HashSet[Int]](mutable.HashSet[Int]())
-    InputData.fromString(orderingRulesString).linesAs[OrderingRule]
-      .foreach(rule => orderingRulesBuilder.getOrCreate(rule.second).addOne(rule.first))
-    val orderingRules = mutable.HashMap.newBuilder[Int, mutable.HashSet[Int]].addAll(orderingRulesBuilder.toMap).result()
-
-    updateString.linesIterator
-      .flatMap: line =>
-        val numbers = line.split(",").map(_.toInt).toSeq
-        val numbersSet = numbers.toSet
-        val filteredRules = orderingRules.flatMap: (key, values) =>
-          if numbersSet.contains(key) then
-            Some(key -> values.intersect(numbersSet))
-          else
-            None
-        val sorted = mutable.ArrayBuffer[Int]()
-        while filteredRules.nonEmpty do
-          val next = filteredRules.filter((key, values) => values.isEmpty).iterator
-          val nextNumber = if next.hasNext then next.next()._1 else (numbersSet -- filteredRules.keys).iterator.next()
-          sorted.addOne(nextNumber)
-          filteredRules.remove(nextNumber)
-          filteredRules.values.foreach(values => values.remove(nextNumber))
-
-        if sorted == numbers then
-          Some(sorted(sorted.size / 2))
-        else
-          None
-      .sum
+    solve(input, equal = true)
 
   def part2(input: InputData): Int =
+    solve(input, equal = false)
+
+  private def solve(input: InputData, equal: Boolean): Int =
     val Array(orderingRulesString, updateString) = input.whole.split("\n\n")
     val orderingRulesBuilder = AutoMap[Int, mutable.HashSet[Int]](mutable.HashSet[Int]())
     InputData.fromString(orderingRulesString).linesAs[OrderingRule]
@@ -59,7 +35,7 @@ object Day05:
           filteredRules.remove(nextNumber)
           filteredRules.values.foreach(values => values.remove(nextNumber))
 
-        if sorted != numbers then
+        if (sorted == numbers) == equal then
           Some(sorted(sorted.size / 2))
         else
           None
