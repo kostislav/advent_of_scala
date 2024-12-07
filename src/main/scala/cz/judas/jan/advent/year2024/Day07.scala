64d36a955e91e715c5c0ba124a6b36e0ee37be73
Jan Judas <jan.judas@resistant.ai>
2024-12-07 07:06:49 +0100

    clean up a bit

--- src/main/scala/cz/judas/jan/advent/year2024/Day07.scala
+++ src/main/scala/cz/judas/jan/advent/year2024/Day07.scala
@@ -4,40 +4,27 @@ import cz.judas.jan.advent.InputData
 
 object Day07:
   def part1(input: InputData): Long =
-    input.lines
-      .flatMap: line =>
-        val Array(testValueString, numberString) = line.split(": ")
-        val testValue = testValueString.toLong
-        val numbers = numberString.split(" ").map(_.toLong)
-        if bleh(testValue, 0, numbers, 0) then
-          Some(testValue)
-        else
-          None
-      .sum
+    solve(input, concatenate = false)
 
   def part2(input: InputData): Long =
+    solve(input, concatenate = true)
+
+  private def solve(input: InputData, concatenate: Boolean): Long =
     input.lines
       .flatMap: line =>
         val Array(testValueString, numberString) = line.split(": ")
         val testValue = testValueString.toLong
         val numbers = numberString.split(" ").map(_.toLong)
-        if bleh2(testValue, 0, numbers, 0) then
-          Some(testValue)
-        else
-          None
+        Some(testValue).filter(bleh(_, 0, numbers, 0, concatenate))
       .sum
 
-  private def bleh(targetResult: Long, partialResult: Long, numbers: IndexedSeq[Long], position: Int): Boolean =
-    if position == numbers.size then
+  private def bleh(targetResult: Long, partialResult: Long, numbers: IndexedSeq[Long], position: Int, concatenate: Boolean): Boolean =
+    if partialResult > targetResult then
+      false
+    else if position == numbers.size then
       partialResult == targetResult
     else
-      bleh(targetResult, partialResult + numbers(position), numbers, position + 1) || bleh(targetResult, partialResult * numbers(position), numbers, position + 1)
-
-  private def bleh2(targetResult: Long, partialResult: Long, numbers: IndexedSeq[Long], position: Int): Boolean =
-    if position == numbers.size then
-      partialResult == targetResult
-    else
-      bleh2(targetResult, partialResult + numbers(position), numbers, position + 1)
-        || bleh2(targetResult, partialResult * numbers(position), numbers, position + 1)
-        || bleh2(targetResult, s"${partialResult}${numbers(position)}".toLong, numbers, position + 1)
+      bleh(targetResult, partialResult + numbers(position), numbers, position + 1, concatenate)
+        || bleh(targetResult, partialResult * numbers(position), numbers, position + 1, concatenate)
+        || (concatenate && bleh(targetResult, s"${partialResult}${numbers(position)}".toLong, numbers, position + 1, concatenate))
 
