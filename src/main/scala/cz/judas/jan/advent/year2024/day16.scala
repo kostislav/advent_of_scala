78e03bdf25bc89e9f57a3c5966e68e3c2fa40ee7
Jan Judas <jan.judas@resistant.ai>
2024-12-18 21:20:08 +0100

    slight cleanup

--- src/main/scala/cz/judas/jan/advent/year2024/day16.scala
+++ src/main/scala/cz/judas/jan/advent/year2024/day16.scala
@@ -1,6 +1,6 @@
 package cz.judas.jan.advent.year2024
 
-import cz.judas.jan.advent.{Array2d, InputData, Position, RelativePosition, getOnlyElement, shortestPath}
+import cz.judas.jan.advent.{Array2d, AutoMap, InputData, Position, RelativePosition, getOnlyElement, shortestPath}
 
 import scala.collection.mutable
 
@@ -17,44 +17,43 @@ object Day16:
     val targetPosition = maze.indices.filter(position => maze(position) == 'E').getOnlyElement // TODO dedup?
 
     val toVisit = mutable.PriorityQueue[(State, Int)]()(Ordering.by[(State, Int), Int](_._2).reverse)
-    val dist = mutable.HashMap[State, ReverseNeighbors]()
+    val dist = mutable.HashMap[State, Int]()
+    val prev = AutoMap[State, mutable.HashSet[State]](_ => mutable.HashSet())
     var result: Option[Int] = None
     toVisit.addOne((start, 0))
-    dist.put(start, ReverseNeighbors(0))
+    dist.put(start, 0)
     while toVisit.nonEmpty do
       val (closest, distance) = toVisit.dequeue()
       if closest.position == targetPosition then
         result = Some(distance)
-      else if dist(closest).distance == distance then
+      else if dist(closest) == distance then
         neighbors(maze, closest).iterator.foreach: (neighbor, weight) =>
           val neighborWeight = distance + weight
           if result.forall(_ >= neighborWeight) then
-            if !dist.contains(neighbor) then
-              dist.put(neighbor, ReverseNeighbors(neighborWeight))
+            val previousNeighborWeight = dist.get(neighbor)
+            if previousNeighborWeight.forall(_ >= neighborWeight) then
               toVisit.addOne((neighbor, neighborWeight))
-            else
-              if dist(neighbor).distance > neighborWeight then
-                toVisit.addOne((neighbor, neighborWeight))
-                dist(neighbor).distance = neighborWeight
-            dist(neighbor).parents += closest
+              val reverseNeighbors = prev.getOrCreate(neighbor)
+              if previousNeighborWeight.exists(_ > neighborWeight) then
+                reverseNeighbors.clear()
+              dist.put(neighbor, neighborWeight)
+              reverseNeighbors.addOne(closest)
 
     RelativePosition.horizontalDirections.map(State(targetPosition, _)).flatMap: x =>
       if dist.contains(x) then
-        bleh(maze, x, dist, Seq.empty[State]).flatten
+        bleh(maze, x, dist, prev.toMap, Seq.empty[State]).flatten
       else
         Seq.empty
     .toSet.map(_.position).size
 
 
-  private def bleh(maze: Array2d, node: State, dist: mutable.Map[State, ReverseNeighbors], path: Seq[State]): Seq[Seq[State]] =
-    val currentDist = dist(node)
-    if currentDist.distance == 0 then
+  private def bleh(maze: Array2d, node: State, dist: mutable.Map[State, Int], prev: Map[State, mutable.Set[State]], path: Seq[State]): Seq[Seq[State]] =
+    if dist(node) == 0 then
       Seq(path :+ node)
     else
-      currentDist.parents
+      prev(node)
         .toSeq
-        .filter(parent => dist(parent).distance + neighbors(maze, parent).toMap.apply(node) == currentDist.distance)
-        .flatMap(parent => bleh(maze, parent, dist, path :+ node))
+        .flatMap(parent => bleh(maze, parent, dist, prev, path :+ node))
 
   private def neighbors(maze: Array2d, current: State): Iterator[(State, Int)] =
     val State(position, direction) = current
@@ -67,7 +66,3 @@ object Day16:
 
 
 case class State(position: Position, direction: RelativePosition)
-
-
-class ReverseNeighbors(var distance: Int):
-  val parents: mutable.Set[State] = mutable.HashSet()
