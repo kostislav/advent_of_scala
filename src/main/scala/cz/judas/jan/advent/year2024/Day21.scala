19955b18eff9d227e54970597bb40540b70a0491
Jan Judas <jan.judas@resistant.ai>
2024-12-22 13:16:50 +0100

    day 21 part 2, still a bit of duplication

--- src/main/scala/cz/judas/jan/advent/year2024/Day21.scala
+++ src/main/scala/cz/judas/jan/advent/year2024/Day21.scala
@@ -1,53 +1,60 @@
 package cz.judas.jan.advent.year2024
 
-import cz.judas.jan.advent.{Array2d, InputData, Position, RelativePosition, shortestPath}
-
-import scala.annotation.tailrec
+import cz.judas.jan.advent.{Array2d, InputData, Position, RelativePosition, cartesianProduct}
 
 object Day21:
-  def part1(input: InputData): Int =
+  def part1(input: InputData): Long =
+    solve(input, 2)
+
+  def part2(input: InputData): Long =
+    solve(input, 25)
+
+  private def solve(input: InputData, numIntermediateRobots: Int): Long =
     val numericKeypad = Array2d.fromRows("789", "456", "123", ".0A")
     val directionalKeypad = Array2d.fromRows(".^A", "<v>")
 
-    var state = IndexedSeq(Position(3, 2), Position(0, 2), Position(0, 2))
+    val transitions = Seq('<', '>', '^', 'v', 'A')
+      .cartesianProduct(onlyDifferent = false)
+      .map((start, end) => s"${start}${end}" -> possibleMoves(directionalKeypad.positionOfOnly(start), directionalKeypad.positionOfOnly(end), directionalKeypad))
+      .toMap
+
+    val finalCosts = (0 until numIntermediateRobots)
+      .foldLeft(transitions.view.mapValues(_ => 1L).toMap): (costs, _) =>
+        transitions
+          .view.mapValues: options =>
+            options
+              .map: moves =>
+                ("A" + moves).sliding(2).map(ends => costs(ends)).sum
+              .min
+          .toMap
 
     input.lines
       .map: line =>
-        var numMoves = 0
-        line.foreach: c =>
-          val endState = IndexedSeq(numericKeypad.positionOfOnly(c)) ++ IndexedSeq.fill(2)(Position(0, 2))
-//          TODO use the other one
-          numMoves += 1 + shortestPath
-            (state)
-            (_ == endState)
-            (state =>
-              Seq('^', 'v', '>', '<', 'A').flatMap(key => transition(numericKeypad, directionalKeypad, state, 2, key)).map(_ -> 1)
-            )
-            .get
-          state = endState
-        numMoves * line.replace("A", "").toInt
+        val numPushes = ("A" + line)
+          .sliding(2)
+          .map: ends =>
+            possibleMoves(numericKeypad.positionOfOnly(ends(0)), numericKeypad.positionOfOnly(ends(1)), numericKeypad)
+              .map: option =>
+                ("A" + option).sliding(2).map(ends => finalCosts(ends)).sum
+              .min
+          .sum  // TODO dedup
+        numPushes * line.replace("A", "").toInt
       .sum
 
-  def part2(input: InputData): Int =
-    0
-
-  @tailrec
-  private def transition(numericKeypad: Array2d, directionalKeypad: Array2d, state: IndexedSeq[Position], index: Int, keyPress: Char): Option[IndexedSeq[Position]] =
-    def move(direction: RelativePosition): Option[IndexedSeq[Position]] =
-      val newPosition = state(index) + direction
-      val keypad = if index == 0 then numericKeypad else directionalKeypad
-      if keypad.get(newPosition).exists(_ != '.') then
-        Some(state.updated(index, newPosition))
+  private def possibleMoves(start: Position, end: Position, keypad: Array2d): Seq[String] =
+    if keypad(start) == '.' then
+      Seq.empty
+    else
+      if start == end then
+        Seq("A")
       else
-        None
-
-    keyPress match
-      case '^' => move(RelativePosition.UP)
-      case '>' => move(RelativePosition.RIGHT)
-      case 'v' => move(RelativePosition.DOWN)
-      case '<' => move(RelativePosition.LEFT)
-      case 'A' =>
-        if index == 0 then
-          None
-        else
-          transition(numericKeypad, directionalKeypad, state, index - 1, directionalKeypad(state(index)))
+        val result = Seq.newBuilder[String]
+        if start.column < end.column then
+          result ++= possibleMoves(start + RelativePosition.RIGHT, end, keypad).map(">" + _)
+        if start.column > end.column then
+          result ++= possibleMoves(start + RelativePosition.LEFT, end, keypad).map("<" + _)
+        if start.row < end.row then
+          result ++= possibleMoves(start + RelativePosition.DOWN, end, keypad).map("v" + _)
+        if start.row > end.row then
+          result ++= possibleMoves(start + RelativePosition.UP, end, keypad).map("^" + _)
+        result.result()
