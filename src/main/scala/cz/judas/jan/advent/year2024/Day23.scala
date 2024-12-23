5407959f24639927ab2be873a3d70943c52e6982
Jan Judas <jan.judas@resistant.ai>
2024-12-23 07:59:47 +0100

    move reusable things

--- src/main/scala/cz/judas/jan/advent/year2024/Day23.scala
+++ src/main/scala/cz/judas/jan/advent/year2024/Day23.scala
@@ -1,6 +1,6 @@
 package cz.judas.jan.advent.year2024
 
-import cz.judas.jan.advent.{AutoMap, InputData, splitOnce, transformValues}
+import cz.judas.jan.advent.{AutoMap, InputData, UndirectedGraph, maximumClique, splitOnce}
 
 import scala.collection.mutable
 
@@ -31,45 +31,3 @@ object Day23:
       .toSeq
       .sorted
       .mkString(",")
-
-
-def maximumClique[T](graph: UndirectedGraph[T]): Set[T] =
-  def bleh(r: Set[T], p: mutable.Set[T], x: mutable.Set[T]): Option[Set[T]] =
-    if p.isEmpty && x.isEmpty then
-      Some(r)
-    else
-      p
-        .iterator
-        .flatMap: v =>
-          val neighbors = graph.neighborsOf(v)
-          val done = bleh(r + v, mutable.HashSet.from(p).intersect(neighbors), mutable.HashSet.from(p).intersect(neighbors))
-          p -= v
-          x -= v
-          done
-        .maxByOption(_.size)
-
-  bleh(Set.empty, mutable.HashSet.from(graph.nodes), mutable.HashSet()).getOrElse(Set.empty)
-
-
-class UndirectedGraph[T](neighbors: Map[T, Set[T]]):
-  def nodes: Set[T] =
-    neighbors.keys.toSet
-
-  def neighborsOf(node: T): Set[T] =
-    neighbors(node)
-
-object UndirectedGraph:
-  def builder[T]: Builder[T] =
-    Builder()
-
-  class Builder[T]:
-    private val neighbors = AutoMap[T, mutable.Set[T]](_ => mutable.HashSet())
-
-    def addEdge(node1: T, node2: T): Unit =
-      neighbors.getOrCreate(node1) += node2
-      neighbors.getOrCreate(node2) += node1
-
-    def build: UndirectedGraph[T] =
-      UndirectedGraph(
-        neighbors.toMap.transformValues(_.toSet)
-      )
