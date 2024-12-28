4c5b37ca1fb5443046d3484c4ee16bfecb01cb6c
Jan Judas <jan.judas@resistant.ai>
2024-12-28 17:51:38 +0100

    measure running time without file I/O

--- src/main/scala/cz/judas/jan/advent/runner.scala
+++ src/main/scala/cz/judas/jan/advent/runner.scala
@@ -3,24 +3,45 @@ package cz.judas.jan.advent
 import scala.quoted.{Expr, Quotes}
 
 
-inline def run(inline year: Int, inline day: Int, inline part: Int): Any =
+inline def run(inline year: Int, inline day: Int, inline part: Int): (Any, Long) =
   ${ runImpl('year, 'day, 'part) }
 
-def runImpl(year: Expr[Int], day: Expr[Int], part: Expr[Int])(using q: Quotes): Expr[Any] =
+
+private def runImpl(year: Expr[Int], day: Expr[Int], part: Expr[Int])(using q: Quotes): Expr[(Any, Long)] =
   import q.reflect.*
   val constYear = extractConstant(year)
   val constDay = extractConstant(day)
   val constPart = extractConstant(part)
 
-  Select.overloaded(
-    Ref(Symbol.requiredModule(f"cz.judas.jan.advent.year${constYear}.Day$constDay%02d")),
-    s"part${constPart}",
-    List.empty,
-    List('{ InputData.real(${year}, ${day}) }.asTerm)
-  ).asExprOf[Any]
+  val call = Lambda(
+    Symbol.spliceOwner,
+    MethodType(List("input"))(_ => List(TypeRepr.of[InputData]), _ => TypeRepr.of[Any]), {
+      case (_, List(inputExpr: Term)) =>
+        Select.overloaded(
+          Ref(Symbol.requiredModule(f"cz.judas.jan.advent.year${constYear}.Day$constDay%02d")),
+          s"part${constPart}",
+          List.empty,
+          List(inputExpr)
+        )
+      case _ => throw RuntimeException("Oops")
+    }
+  )
 
-def extractConstant(value: Expr[Int])(using q: Quotes): Int =
+  '{
+    val input = InputData.real(${ year }, ${ day })
+    runInternal(input, ${ call.asExprOf[InputData => Any] })
+  }
+
+
+private def extractConstant(value: Expr[Int])(using q: Quotes): Int =
   import q.reflect.*
   value.asTerm match
     case Inlined(_, _, Literal(IntConstant(constValue))) => constValue
     case _ => report.errorAndAbort("Expected constant value")
+
+
+private def runInternal(input: InputData, puzzle: InputData => Any): (Any, Long) =
+  val startTime = System.currentTimeMillis()
+  val result = puzzle(input)
+  val endTime = System.currentTimeMillis()
+  (result, endTime - startTime)
