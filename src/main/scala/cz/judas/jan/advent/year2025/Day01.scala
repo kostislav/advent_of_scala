42ae090cec93179a8fdad99dd366932e53f10969
Jan Judas <jan.judas@resistant.ai>
2025-12-01 16:16:15 +0100

    clean up

--- src/main/scala/cz/judas/jan/advent/year2025/Day01.scala
+++ src/main/scala/cz/judas/jan/advent/year2025/Day01.scala
@@ -2,6 +2,8 @@ package cz.judas.jan.advent.year2025
 
 import cz.judas.jan.advent.{InputData, pattern}
 
+import scala.math.floorMod
+
 object Day01:
   def part1(input: InputData): Int =
     input.linesAs[Movement]
@@ -12,22 +14,18 @@ object Day01:
       .count(_ == 0)
 
   def part2(input: InputData): Int =
-    var dial = 50
-    var numClicks = 0
-    input.linesAs[Movement].foreach: movement =>
-      movement.direction match
-        case Direction.Left =>
-          dial = dial - movement.amount
-          while dial < 0 do
-            dial += 100
-            numClicks += 1
-        case Direction.Right =>
-          dial = dial + movement.amount
-          while dial > 99 do
-            dial -= 100
-            numClicks += 1
-
-    numClicks
+    input.linesAs[Movement]
+      .foldLeft(dial = 50, clicks = 0): (current, movement) =>
+        val numFullRotations = movement.amount / 100
+        val remainingAmount = movement.amount % 100
+        val newDial = movement.direction match
+          case Direction.Left =>
+            (if current.dial == 0 then 100 else current.dial) - remainingAmount
+          case Direction.Right =>
+            current.dial + remainingAmount
+        val extraClick = if newDial <= 0 || newDial >= 100 then 1 else 0
+        (dial = floorMod(newDial, 100), clicks = current.clicks + numFullRotations + extraClick)
+      .clicks
 
 
 enum Direction:
