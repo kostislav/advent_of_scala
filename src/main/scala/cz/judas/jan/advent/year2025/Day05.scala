f9890c607e15eda5c64c9bf94bbfdcf12f3a6d02
Jan Judas <jan.judas@resistant.ai>
2025-12-05 07:45:46 +0100

    inefficient, but no external libraries

--- src/main/scala/cz/judas/jan/advent/year2025/Day05.scala
+++ src/main/scala/cz/judas/jan/advent/year2025/Day05.scala
@@ -1,25 +1,46 @@
 package cz.judas.jan.advent.year2025
 
-import com.google.common.collect.TreeRangeSet
 import cz.judas.jan.advent.{InputData, header, lines, pattern}
-
-import scala.jdk.CollectionConverters.{IterableHasAsScala, SeqHasAsJava}
 
 object Day05:
   def part1(input: InputData): Long =
-    val (freshRanges, ids) = input.wholeAs[Seq[Range2] @lines @header, Seq[Long] @lines]
+    val (freshRanges, ids) = parseInput(input)
 
     ids
       .count(id => freshRanges.exists(_.contains(id)))
 
   def part2(input: InputData): Long =
-    val (freshRanges, _) = input.wholeAs[Seq[Range2] @lines @header, Seq[String] @lines]
+    val (freshRanges, _) = parseInput(input)
 
-    val xex = TreeRangeSet.create(freshRanges.map(range => com.google.common.collect.Range.closed(range.start, range.end)).asJava)
-    xex.asRanges().asScala.map(range => range.upperEndpoint() - range.lowerEndpoint() + 1).sum
+    freshRanges.foldLeft(RangeSet.empty)(_ + _)
+      .map(_.size)
+      .sum
+
+  private def parseInput(input: InputData): (Seq[Range2], Seq[Long]) =
+    input.wholeAs[Seq[Range2] @lines @header, Seq[Long] @lines]
 
 
 @pattern("{}-{}")
 case class Range2(start: Long, end: Long):
   def contains(id: Long): Boolean =
     id >= start && id <= end
+
+  def overlaps(other: Range2): Boolean =
+    this.end >= other.start && other.end >= this.start
+
+  def size: Long =
+    end - start + 1
+
+
+class RangeSet private(ranges: Set[Range2]) extends Iterable[Range2]:
+  def +(range: Range2): RangeSet =
+    val overlapping = ranges.filter(_.overlaps(range))
+    val toMerge = overlapping + range
+    RangeSet(ranges -- overlapping + Range2(toMerge.map(_.start).min, toMerge.map(_.end).max))
+
+  override def iterator: Iterator[Range2] =
+    ranges.iterator
+
+
+object RangeSet:
+  def empty: RangeSet = RangeSet(Set.empty)
