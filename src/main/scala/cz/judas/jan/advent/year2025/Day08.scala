4a45f4278c5f11627c3aae7bdd27b642e6089786
Jan Judas <jan.judas@resistant.ai>
2025-12-08 08:00:07 +0100

    clean up

--- src/main/scala/cz/judas/jan/advent/year2025/Day08.scala
+++ src/main/scala/cz/judas/jan/advent/year2025/Day08.scala
@@ -10,64 +10,69 @@ object Day08:
 
   def part1(input: InputData, numConnections: Int): Long =
     val boxes = input.linesAs[Position3d].toSet
-    val edges = boxes.flatMap(box1 => boxes.filter(box2 => box2 != box1).map(box2 => Set(box1, box2)))
-      .toSeq
-      .sortBy(length)
-      .iterator
-    val componentByBox = mutable.HashMap[Position3d, Int]()
-    val boxesByComponents = mutable.HashMap[Int, Set[Position3d]]()
-    boxes.zipWithIndex.foreach: (box, id) =>
-      componentByBox.put(box, id)
-      boxesByComponents.put(id, Set(box))
-
-    val processed = mutable.HashSet[Set[Position3d]]()
+    val edgeIterator = edgesByDistance(boxes).iterator
+    val graph = ConnectedComponents[Position3d]()
+    boxes.foreach(graph.addNode)
 
     (0 until numConnections).foreach: _ =>
-      val closestPair = edges.next().toSeq
-      val box1 = closestPair(0)
-      val box2 = closestPair(1)
-      val component1Id = componentByBox(box1)
-      val component2Id = componentByBox(box2)
-      if component1Id != component2Id then
-        boxesByComponents(component2Id).foreach(box => componentByBox.put(box, component1Id))
-        boxesByComponents.put(component1Id, boxesByComponents(component1Id) ++ boxesByComponents(component2Id))
-        boxesByComponents.remove(component2Id)
+      val shortestEdge = edgeIterator.next()
+      graph.addEdge(shortestEdge)
 
-    boxesByComponents.values.toSeq.map(_.size).sorted.reverse.take(3).product
+    graph.components.toSeq.map(_.size).sorted.reverse.take(3).product
 
   def part2(input: InputData): Long =
     val boxes = input.linesAs[Position3d].toSet
-    val edges = boxes.flatMap(box1 => boxes.filter(box2 => box2 != box1).map(box2 => Set(box1, box2)))
-      .toSeq
-      .sortBy(length)
-      .iterator
+    val edgeIterator = edgesByDistance(boxes).iterator
 
-    val componentByBox = mutable.HashMap[Position3d, Int]()
-    val boxesByComponents = mutable.HashMap[Int, Set[Position3d]]()
-    boxes.zipWithIndex.foreach: (box, id) =>
-      componentByBox.put(box, id)
-      boxesByComponents.put(id, Set(box))
-
-    val processed = mutable.HashSet[Set[Position3d]]()
+    val graph = ConnectedComponents[Position3d]()
+    boxes.foreach(graph.addNode)
 
     var bleh = 0L
-    while boxesByComponents.size > 1 do
-      val closestPair = edges.next().toSeq
-      val box1 = closestPair(0)
-      val box2 = closestPair(1)
-      val component1Id = componentByBox(box1)
-      val component2Id = componentByBox(box2)
-      if component1Id != component2Id then
-        boxesByComponents(component2Id).foreach(box => componentByBox.put(box, component1Id))
-        boxesByComponents.put(component1Id, boxesByComponents(component1Id) ++ boxesByComponents(component2Id))
-        boxesByComponents.remove(component2Id)
-        bleh = box1.x * box2.x
+    while graph.numComponents > 1 do
+      val shortestEdge = edgeIterator.next()
+      graph.addEdge(shortestEdge)
+      bleh = shortestEdge.node1.x * shortestEdge.node2.x
 
     bleh
 
-  private def length(edge: Set[Position3d]): Long =
-    val bleh = edge.toList
-    bleh(0).squareDistance(bleh(1))
+  private def edgesByDistance(boxes: Iterable[Position3d]): Seq[UndirectedEdge[Position3d]] =
+    boxes.flatMap(box1 => boxes.filter(box2 => box2 != box1).map(box2 => UndirectedEdge(box1, box2)))
+      .toSeq
+      .sortBy(edge => edge.node1.squareDistance(edge.node2))
+
+
+class ConnectedComponents[T]:
+  private val componentByNode = mutable.HashMap[T, Int]()
+  private val nodesByComponent = mutable.HashMap[Int, Set[T]]()
+
+  def addNode(node: T): Unit =
+    val id = nodesByComponent.size + 1
+    componentByNode.put(node, id)
+    nodesByComponent.put(id, Set(node))
+
+  def addEdge(edge: UndirectedEdge[T]): Unit =
+    val component1Id = componentByNode(edge.node1)
+    val component2Id = componentByNode(edge.node2)
+    if component1Id != component2Id then
+      nodesByComponent(component2Id).foreach(node => componentByNode.put(node, component1Id))
+      nodesByComponent.put(component1Id, nodesByComponent(component1Id) ++ nodesByComponent(component2Id)) // TODO mutable set
+      nodesByComponent.remove(component2Id)
+
+  def numComponents: Int =
+    nodesByComponent.size
+
+  def components: Iterable[Set[T]] =
+    nodesByComponent.values
+
+
+class UndirectedEdge[T](val node1: T, val node2: T):
+  override def equals(o: Any): Boolean =
+    val other = o.asInstanceOf[UndirectedEdge[T]]
+    (other.node1 == this.node1 && other.node2 == this.node2) || (other.node2 == this.node1 && other.node1 == this.node2)
+
+  override def hashCode(): Int =
+    node1.hashCode() * node2.hashCode()
+
 
 @pattern("{},{},{}")
 case class Position3d(x: Long, y: Long, z: Long):
